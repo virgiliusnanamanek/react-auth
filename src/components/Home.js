e494b846a8da8349ad18aa250572435c3023ea3a
virgiliusnanamanek <vickynana034@gmail.com>
2022-09-14 15:17:45 +0700

    update input placeholder

--- src/components/Home.js
+++ src/components/Home.js
@@ -58,7 +58,7 @@ function Home() {
                 type="search"
                 id="default-search"
                 className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Search any film"
+                placeholder= "Masukan kata kunci"
                 required=""
                 autoComplete="off"
                 onChange={onInputChange}
