55b03f50e28f56a32029a7bcc3a3c3f302616988
virgiliusnanamanek <vickynana034@gmail.com>
2022-09-14 14:42:26 +0700

    update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -60,6 +60,7 @@ function Home() {
                 className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Search any film"
                 required=""
+                autoComplete="off"
                 onChange={onInputChange}
               />
               <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Search</button>
